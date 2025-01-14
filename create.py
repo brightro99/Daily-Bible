3e3f44216ff5916ee4c3a0b63db96a8c9031537b
brightro99 <brightro99@gmail.com>
2025-01-14 09:14:46 +0900

    Add Ezekiel 1

--- create.py
+++ create.py
@@ -57,5 +57,5 @@ if __name__ == "__main__":
     # folder_name = sys.argv[1]
     # chapter_number = sys.argv[2]
     # create_job_chapter_md(folder_name, chapter_number)
-    folder_name = "예레미야애가"
+    folder_name = "에스겔"
     create_job_chapter_md(folder_name)
