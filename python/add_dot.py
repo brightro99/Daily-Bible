8de1e4b183e303162b9a17f118ba417601ceeacb
brightro99 <brightro99@gmail.com>
2025-12-31 14:58:11 +0900

    Add 1 Peter 1

--- python/add_dot.py
+++ python/add_dot.py
@@ -4,7 +4,7 @@ import re
 # 상위 성경 폴더명 (고정값)
 BASE_DIR = "쉬운성경"
 # 현재 작업할 성경 책 이름
-FOLDER_NAME = "야고보서"
+FOLDER_NAME = "베드로전서"
 
 
 def get_chapter_number():
--- python/create.py
+++ python/create.py
@@ -3,7 +3,7 @@ import os
 # 상위 성경 폴더
 BASE_DIR = "쉬운성경"
 # 작업할 성경 책 이름
-FOLDER_NAME = "야고보서"
+FOLDER_NAME = "베드로전서"
 
 
 def get_next_chapter_number():
